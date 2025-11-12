805b0358bc50e18b8febe8112ac193e84da3a72a
jhwan5 <jjhwxng@gmail.com>
2025-11-12 00:30:12 -0600

    base project

---
+++ src/components/Camera.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Camera() {
+  return (
+    <div className="page">
+      <h1>Camera Stream</h1>
+      <p>This page will show the live feed.</p>
+    </div>
+  );
+}
+
+export default Camera;
---
+++ src/components/GPS.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GPS() {
+  return (
+    <div className="page">
+      <h1>GPS Tracking</h1>
+      <p>This page will visualize real-time GPS location data.</p>
+    </div>
+  );
+}
+
+export default GPS;
